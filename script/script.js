140e644f911873a7e064cd1bbb981d5cd744c901
Sayed Kashif <sayedkaskai22@gmail.com>
2023-10-08 15:59:19 +0200

    this is a confetti webite

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+    }).then(() => jsConfetti.addConfetti())
+})
